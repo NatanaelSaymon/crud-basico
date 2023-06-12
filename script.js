3dbe0daa770834ffc4f9561e0e83721d0479f2c6
Natanael Saymon <saymoneo@gmail.com>
2023-06-11 22:16:05 -0300

    feat: funções de editar, deletar - crud finalizado

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ function crud() {
     const openModal = document.querySelector('#open-modal');
     const closeModal = document.querySelector('#btn-close');
     const btn_add = document.querySelector("#btn-add")
-    const productId = document.querySelector("#id")
-    const productName = document.querySelector("#produto")
-    const productPrice = document.querySelector("#valor")
     const tbody = document.querySelector('#table-body')
-    const editProduct = document.querySelector('#editProduct')
 
 
     function open_modal() {
@@ -18,10 +14,11 @@ function crud() {
 
     function close_modal(){
         modal.classList.remove('is-active')
+        clearFields()
     }
 
     function deleteProduct(index) {
-        const db_product = readyProduct()
+        const db_product = JSON.parse(localStorage.getItem('db_product'))
         db_product.splice(index, 1)
         localStorage.setItem('db_product', JSON.stringify(db_product))
         readyProduct()
@@ -31,6 +28,7 @@ function crud() {
         const db_product = JSON.parse(localStorage.getItem('db_product'))
         db_product[index] = product
         localStorage.setItem('db_product', JSON.stringify(db_product))
+        readyProduct()
     }
 
     function readyProduct(){
@@ -41,10 +39,14 @@ function crud() {
                     <tr>
                         <td>${index}</td>
                         <td>${item.product}</td>
-                        <td>${item.price}</td>
-                        <td>
-                            <button id="editProduct"><img src="./img/editar.png" alt="editar"></button>
-                            <button id="deleteProduct"><img src="./img/deletar.png" alt="deletar"></button>
+                        <td>R$ ${item.price}</td>
+                        <td style="padding: 5px 0;">
+                            <button type="button" id="edit-${index}" class="btn-edit">
+                                Editar
+                            </button>
+                            <button type="button" id="delete-${index}" class="btn-delete">
+                                Excluir
+                            </button>
                         </td>
                     </tr>
                 `
@@ -59,26 +61,77 @@ function crud() {
         localStorage.setItem('db_product', JSON.stringify(db_product))
     }
 
-    function saveProduct() {
-        const id = productId.value;
-        const product = productName.value;
-        const price = productPrice.value;
+    function clearFields() {
+        const fields = document.querySelectorAll('.field')
+        fields.forEach(item => item.value = '')
+    }
 
-        const item = {
-            id: id,
-            product: product,
-            price: price
+    function saveProduct() {
+        const produtId = document.querySelector("#id").value
+        const productName = document.querySelector("#produto").value
+        const ProductPrice = document.querySelector("#valor").value
+        const index = document.querySelector('#index').dataset.index
+
+        const product = {
+            id: produtId,
+            product: productName,
+            price: ProductPrice
         }
 
-        createProduct(item)
-        readyProduct()
-        close_modal()
+        if(index === 'new') {
+            if(produtId === '' || productName === '' || ProductPrice === '') {
+                return alert('Preencha todos os campos!')
+            } 
+    
+            createProduct(item)
+            readyProduct()
+            clearFields()
+            close_modal()
+
+        } else {
+            updateProduct(index, product)
+            close_modal()
+        }
+    }
+
+    function fillFields(product) {
+        document.querySelector("#id").value = product.id
+        document.querySelector("#produto").value = product.product
+        document.querySelector("#valor").value = product.price
+        document.querySelector("#index").dataset.index = product.index
+    }
+
+    function editProduct(index) {
+        const dados = JSON.parse(localStorage.getItem('db_product'))
+        
+        const product = dados[index]
+        product.index = index
+        fillFields(product)
+        open_modal()
+    }
+
+    function action(e) {
+        if(e.target.type == 'button') {
+            const [action, index] = e.target.id.split('-')
+
+            if(action === 'edit') {
+                editProduct(index)
+            } else {
+                const product = JSON.parse(localStorage.getItem('db_product'))[index]
+                const response = confirm(`Deseja realmente excluir o produto: ${product.product}?`)
+                if(response) {
+                    deleteProduct(index)
+                }
+                
+            }
+        }
     }
 
     //eventos
     openModal.addEventListener('click', open_modal)
     closeModal.addEventListener('click', close_modal)
     btn_add.addEventListener('click', saveProduct)
+    tbody.addEventListener('click', action)
 
 
     readyProduct()
